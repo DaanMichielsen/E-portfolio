947f0b01a889cf0bd8ddd7085404a7224770590e
DaanMichielsen <r0896365@student.thomasmore.be>
2023-03-29 15:29:51 +0200

    changes footer and about me page

--- resources/views/about_me.blade.php
+++ resources/views/about_me.blade.php
@@ -35,7 +35,7 @@
         </div>
         <a href="../../public/images/CV_Daan_Michielsen.pdf" download class="bg-grey-light hover:bg-grey text-grey-darkest hover:text-teal-300 font-bold py-2 px-4 rounded inline-flex items-center">
             <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
-            <span>Download CV</span>
+            <span>CV</span>
         </a>
     </div>
 </x-e-portfolio-layout>
--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -1,5 +1,8 @@
 <footer class="container mx-auto p-4 text-sm border-t flex justify-between items-center">
     <div>Portfolio - © {{ date('Y') }}</div>
-    <div><a class="hover:text-teal-300" href="https://github.com/DaanMichielsen" target="_blank">Github - <i class="fa-brands fa-github"></i></a></div>
-    <div>Built with Laravel {{ app()->version() }}</div>
+    <div class="flex flex-row">
+        <div><a class="hover:text-teal-300 mx-2" href="https://github.com/DaanMichielsen" target="_blank">Github - <i class="fa-brands fa-github"></i></a></div>
+        <div><a class="hover:text-teal-300 mx-2" href="https://www.linkedin.com/in/daan-michielsen-891577270/">LinkedIn - <i class="fa-brands fa-linkedin"></i></a></div>
+    </div>
+    <div>Built with <a class="hover:text-teal-300" href="https://laravel.com/">Laravel - <i class="fa-brands fa-laravel"></i></a> {{ app()->version() }}</div>
 </footer>
